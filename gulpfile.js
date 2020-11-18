80fc7551a0dd52eabad2dac1f1a43fba06d9042d
Cristian Tosco <cristian.tosco@gmail.com>
2020-11-18 11:30:57 -0300

    Actualizacion de gulp y librerias dependientes

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,31 @@ var cssnano = require('gulp-cssnano');
 var htmlmin = require('gulp-htmlmin');
 var minify = require('gulp-minify');
 var compass = require('gulp-compass');
-const imagemin = require('gulp-imagemin');
-const pngquant = require('imagemin-pngquant');
 var browserSync = require('browser-sync').create();
 var ghPages = require('gulp-gh-pages');
 
+//const { series } = require('gulp');
+//const { watch } = require('gulp');
+const { series, watch, src, dest } = require('gulp');
 
-gulp.task('nunjucks', function() {
-  nunjucksRender.nunjucks.configure(['templates/']);
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const critical = require('critical');
 
+function nunjucksTask(cb) {
   // Gets .html and .nunjucks files in pages
-  return gulp.src('pages/**/*.+(html|nunjucks)')
+   return gulp.src('pages/*.html')
   // Renders template with nunjucks
-  .pipe(nunjucksRender())
-  // output files in .www folder
-  .pipe(gulp.dest('.www'))
-});
+    .pipe(nunjucksRender({
+      path: ['pages/', 'templates/'],
+      watch: false,
+    }))
+  // Gets .html and .nunjucks files in pages
+    .pipe(gulp.dest('.www'));
+  cb();
+}
 
-// gulp.task('default', function () {
-//   return gulp.src('src/templates/*.html')
-//     .pipe(nunjucksRender({
-//       path: ['src/templates/'] // String or Array
-//     }))
-//     .pipe(gulp.dest('dist'));
-// });
-
-gulp.task('compass', function() {
+function compassTask(cb){
   gulp.src('./sass/*.scss')
     .pipe(compass({
       config_file: './config.rb',
@@ -37,62 +36,72 @@ gulp.task('compass', function() {
       sass: 'sass'
     }))
     .pipe(gulp.dest('stylesheets'));
-});
+  cb();
+}
 
-gulp.task('css-minify', function() {
-    return gulp.src('stylesheets/**.css')
-        .pipe(cssnano())
-        .pipe(gulp.dest('.www/css'));
-});
+function cssMinifyTask(cb) {
+  return gulp.src('stylesheets/**.css')
+      .pipe(cssnano())
+      .pipe(gulp.dest('.www/css'));
+  cb();
+}
 
-gulp.task('minify-html', function() {
+function minifyHtmlTask(cb) {
   return gulp.src('./*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('.www'))
-});
+  cb();
+}
 
-gulp.task('compress', function() {
+function compressTask(cb) {
   gulp.src('js/*.js')
     .pipe(minify({
         exclude: ['tasks'],
         ignoreFiles: ['.combo.js', '-min.js']
     }))
     .pipe(gulp.dest('.www/js'))
-});
+  cb();
+}
 
-gulp.task('image-min', () => {
-  return gulp.src('img/*')
+function imageMinTask(cb) {
+  return src('img/*')
     .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
+       progressive: true,
+       svgoPlugins: [{removeViewBox: false}],
       use: [pngquant()]
     }))
-    .pipe(gulp.dest('.www/images'));
-});
-
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
+    .pipe(dest('.www/images'));
+}
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['compass'], function() {
-
+function serveTask(cb) {
     browserSync.init({
         server: ".www"
     });
+    watch(['sass/*.scss'], compassTask, function(cb) {
+      cb();
+    });
+    watch(".www/*.html").on('change', browserSync.reload);
+    cb();
+}
 
-    gulp.watch("sass/*.scss", ['compass']);
-    gulp.watch(".www/*.html").on('change', browserSync.reload);
-});
+function criticalTask(cb) {
+  critical.generate({
+      inline: true,
+      base: './.www/',
+      src: 'index.html',
+      target: 'css/index-critical.css',
+      //target: 'index-critical.html',
+      minify: true
+  });
+  cb();
+}
 
-gulp.task('deploy', function() {
-  return gulp.src('./.www/**/*')
-    .pipe(ghPages({remoteUrl: "git@github.com:ctosco/sietecolores.git"}));
-});
-
-gulp.task('build', ['nunjucks', 'compass', 'css-minify', 'compress', 'image-min']);
+exports.nunjucks = nunjucksTask
+exports.compass = compassTask
+exports.cssMinify = cssMinifyTask
+exports.compress = compressTask
+exports.imagemin = imageMinTask
+exports.serve = serveTask
+exports.critical = criticalTask
+exports.build = series(nunjucksTask, compassTask, cssMinifyTask, compressTask, imageMinTask);
